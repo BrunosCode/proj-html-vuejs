f70baf8a7110236ab58ecbe70dd9d242128ecd0d
brunEdo <edobruno.m@gmail.com>
2021-10-11 17:27:05 +0200

    split hero sections

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
     <Header :header-nav="headerNav" :bg-color="'primary'"/>
     <!-- Header with logo and headerNav component-->
     <main class="c-main">
-      <!-- Hero py-3 -->
+      <SplitHero :content="hero1" :is-reversed="false" class="h-py--4" />
       <!-- GridSection -->
-      <!-- HeroReversed py-2 -->
+      <SplitHero :content="hero2" :is-reversed="true" class="h-py--3" />
       <!-- CaruselSection -->
       <!-- CardsSection -->
       <!-- PostsSection -->
@@ -20,6 +20,8 @@
 import Header from "./components/Header.vue";
 import Footer from "./components/Footer.vue";
 
+import SplitHero from "./components/SplitHero.vue";
+
 import companyInfo from "./data/companyInfo.json";
 import headerNav from "./data/headerNav.json";
 import footerNav from "./data/footerNav.json";
@@ -28,13 +30,55 @@ export default {
   name: 'App',
   components: {
     Header,
-    Footer
+    Footer,
+    SplitHero
   },
   data() {
     return {
       companyInfo,
       headerNav,
-      footerNav
+      footerNav,
+      hero1: {
+        title: "Business planning digital technology modern solution !",
+        text: "When, while the valley teems with vapour around meand the meridian sun strikes the upper surface .",
+        buttons: [
+          {
+            text: "Try it now",
+            link: "#",
+            type: "red"
+          },
+          {
+            text: "Explore More",
+            link: "#",
+            type: "blue"
+          }
+        ],
+        img: {
+          alt: "Man behind the desk",
+          src: "header.png"
+        }
+      },
+      hero2: {
+        pretitle: "Start you project",
+        title: "Grow your business with our strategy",
+        text: "When, while the valley teems with vapour around meand the meridian sun strikes the upper surface of the impenetrable follage of my trees, and but a few stray gleams steal into the inner sanctury.",
+        buttons: [
+          {
+            text: "Try it now",
+            link: "#",
+            type: "red"
+          },
+          {
+            text: "Explore More",
+            link: "#",
+            type: "blue"
+          }
+        ],
+        img: {
+          alt: "Man over stacks",
+          src: "men-data.png"
+        }
+      }
     }
   }
 }
---
+++ src/components/SplitHero.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="c-hero">
+    <div :class="{'l-row--reversed': isReversed}" class="l-container l-row l-alignCenter">
+      <div class="l-col--half">
+        <div class="l-col--2third">
+          <h4 v-if="content.pretitle" class="c-hero__pretitle">{{content.pretitle}}</h4>
+          <h2 v-if="content.title" class="c-hero__title h-mb--2">{{content.title}}</h2>
+          <p v-if="content.text" class="c-hero__text h-mb--4">{{content.text}}</p>
+          <div v-if="content.buttons">
+            <a v-for="(btn, i) in content.buttons" :key="i" 
+            :href="btn.link" :class="`c-btn--${btn.type}`" 
+            class="c-btn h-mr--2">{{btn.text}}</a>
+          </div>
+        </div>
+      </div>
+      <div class="l-col--half h-m--2">
+        <img class="c-hero__img" :src="require(`../assets/images/${content.img.src}`)" :alt="content.img.alt">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'SplitHero',
+  props: {
+    content: Object,
+    isReversed: Boolean,
+  }
+}
+</script>
+
+<style scoped lang="scss">
+@import "../assets/style/variables.scss";
+@import "../assets/style/mixins.scss";
+
+.c-hero {
+  color: $white;
+  @include bgGradient(180deg, $blue, $darkBlue);
+
+  &__title {
+    text-transform: capitalize;
+    font-size: 3rem;
+  }
+}
+
+</style>
