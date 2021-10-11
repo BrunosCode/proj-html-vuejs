1bb48ff5f790f3e634b8417cda631d532b777b68
brunEdo <edobruno.m@gmail.com>
2021-10-11 13:14:26 +0200

    bgGradient Mixin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,18 +1,22 @@
 <template>
-  <header :class="`h-bg--${bgColor}`" class="l-row l-spaceBetween l-alignCenter c-header">
-    <img :src="require(`../assets/images/${logoImg}`)" alt="#" class="c-logo c-header__logo">
-    <nav class="c-header__nav">
-      <ul class="l-row l-alignCenter h-listReset">
-        <li v-for="(link, i) in headerNav.links" :key="i">
-          <a :class="{'h-active': link.text === 'home'}" 
-          :href="link.link" class="c-link c-header__link">{{link.text}}</a>
-        </li>
-        <li v-for="(btn, i) in headerNav.buttons" :key="i">
-          <a :class="`c-btn--${btn.type}`" :href="btn.link" 
-          class="c-btn c-header__btn">{{btn.text}}</a>
-        </li>
-      </ul>
-    </nav>
+  <header :class="`h-bg--${bgColor}`" class="c-header">
+    <div class="l-container l-row l-spaceBetween l-alignCenter">
+      <img :src="require(`../assets/images/${logoImg}`)" alt="#" class="c-logo c-header__logo">
+
+      <nav class="c-header__nav">
+        <!-- Print data from the prop json file -->
+        <ul class="l-row l-alignCenter h-listReset">
+          <li v-for="(link, i) in headerNav.links" :key="i">
+            <a :class="{'h-active': link.text === 'home'}" 
+            :href="link.link" class="c-link c-header__link">{{link.text}}</a>
+          </li>
+          <li v-for="(btn, i) in headerNav.buttons" :key="i">
+            <a :class="`c-btn--${btn.type}`" :href="btn.link" 
+            class="c-btn c-header__link">{{btn.text}}</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   </header>
 </template>
 
@@ -33,6 +37,10 @@ export default {
 .c-header {
   padding: 1rem;
   color: $white;
+
+  &__logo {
+    height: 3rem;
+  }
 
   &__link {
     margin-left: 1rem;
